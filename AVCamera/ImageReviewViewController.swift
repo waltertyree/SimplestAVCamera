3d8fe34e4b6d6ac1343114ca8f7e318e436d285c
Walter Tyree <walter@tyreeapps.com>
2017-12-05 07:38:14 -0500

    add info plist setting

--- AVCamera/ImageReviewViewController.swift
+++ AVCamera/ImageReviewViewController.swift
@@ -12,31 +12,12 @@ class ImageReviewViewController: UIViewController {
 
     @IBOutlet weak var finalImage : UIImageView!
     var imageSource : UIImage?
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.finalImage.image = self.imageSource
                self.finalImage.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi/2))
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
